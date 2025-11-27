372469ee79eb415172a526e3d49b1fcfb48d5363
yzma-er <bumolyadysmael@gmail.com>
2025-11-28 01:03:49 +0800

    Update ManageCarousel.jsx

--- src/pages/ManageCarousel.jsx
+++ src/pages/ManageCarousel.jsx
@@ -65,28 +65,7 @@ function ManageCarousel() {
 
   return (
     <div className="manage-carousel">
-      {/* Back button */}
-      <div style={{ position: "relative", marginBottom: "20px", height: "40px" }}>
-        <button
-          className="back-admin-btn"
-          onClick={() => navigate("/admin")}
-          style={{
-            padding: "6px 12px",
-            borderRadius: "30px",
-            backgroundColor: "#1c7c0f",
-            color: "white",
-            cursor: "pointer",
-            fontSize: "16px",
-            fontWeight: "bold",
-            display: "inline-flex",
-            alignItems: "center",
-            gap: "4px",
-            position: "absolute",
-            left: 0,
-          }}
-        >
-          <span style={{ fontWeight: "bold", fontSize: "18px" }}>←</span>
-        </button>
+      
       <h2>Manage Carousel Images</h2>
 
       <div className="upload-form">
