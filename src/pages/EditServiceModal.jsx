3e4bb49149672dde756285441921c883bbaf9010
yzma-er <bumolyadysmael@gmail.com>
2025-11-27 21:28:28 +0800

    Update EditServiceModal.jsx

--- src/pages/EditServiceModal.jsx
+++ src/pages/EditServiceModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Modal.css";
+import "./Services.css";
 
 function EditServiceModal({ service, onClose, refreshServices }) {
   const backendURL =
